f54c19c6400731de7726f8a859fe8e9b016b5150
Nupurpatwal <137989160+NupurPatwal@users.noreply.github.com>
2026-06-01 18:21:26 +0000

    datatypes done

---
+++ 01Basics/datatypes.py
@@ -0,0 +1,28 @@
+# type = integer
+
+age = 23
+print("The age is :" ,age)
+print(type(age))
+
+# type= String
+name1 = 'Nupur1'
+name2 = "Nupur2"
+name3 = '''Nupur3'''
+print(name1)
+print(name2)
+print(name3)
+
+# type = float
+
+salaryinPune = 2000000.00
+print("Nupur's salary in Pune is:",salaryinPune)
+print(type(salaryinPune))
+
+#type = Boolean
+salary = True
+print("The value of salary is correct which is :", salary)
+
+#None = No value stored
+tension = None
+print("The stress in me is ", tension)
+print(type(tension))
--- 01Basics/varanddt.py
+++ 01Basics/varanddt.py
@@ -1,1 +1,9 @@
 print ("you did it nupur patwal")
+
+name = "Nupur"
+age =24
+salary = 2000000.00
+
+print(type(name))
+print(type(age)) 
+print(type(salary))
